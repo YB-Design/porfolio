b6a6add8df14a5b0361ba544364769c3480c3a7e
yoanbouillon <140432800+yoanbouillon@users.noreply.github.com>
2023-09-02 13:43:49 -0400

    language update

--- script.js
+++ script.js
@@ -26,5 +26,19 @@ window.addEventListener('scroll', function() {
 
 ----------------------------------------------
 
+// language-switcher.js
+
+function setLanguage(language) {
+    // Define a variable to hold the base URL of your website
+    var baseUrl = "https://yb-design.github.io/porfolio/";
+
+    // Set the language by updating the "href" attribute of the language switcher link
+    if (language === "en") {
+        document.getElementById("language-switcher").getElementsByTagName("a")[0].href = baseUrl + "index.html";
+    } else if (language === "fr") {
+        document.getElementById("language-switcher").getElementsByTagName("a")[0].href = baseUrl + "indexfr.html";
+    }
+}
+
 
 
