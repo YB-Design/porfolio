d7bef0d444f124f6b2f3437d2b8489f4d2e73ce0
yoanbouillon <140432800+yoanbouillon@users.noreply.github.com>
2023-09-03 15:02:53 -0400

    traduction temportaire

--- language-switcher.js
+++ language-switcher.js
@@ -1,44 +1,44 @@
+
+
 let languages = {
-    en: {
-        title: "YB DESIGN PORTFOLIO"
-    },
-    fr: {
-        title: "PORTFOLIO YB DESIGN"
-    }
-}
+  en: {
+    title: "YB DESIGN PORTFOLIO"
+  },
+  fr: {
+    title: "PORTFOLIO YB DESIGN"
+  }
+};
 
-document.addEventListener("DOMContentLoaded", () => {
-    let storedLanguage = localStorage.getItem("language");
-    if (storedLanguage) {
-        changeLanguage(storedLanguage);
-    } else {
-        // Set a default language here if needed
-        changeLanguage("en"); // Default to English
-    }
-    [...document.querySelectorAll("[data-change-language]")].forEach((element) => {
-        element.addEventListener("click", (event) => {
-            event.preventDefault();
-            let language = element.getAttribute("data-change-language");
-            localStorage.setItem("language", language);
-            changeLanguage(language);
-        });
-    });
-});
-
+// Function to change the language
 function changeLanguage(language) {
-    console.log("Changing language to:", language); // Debugging line
-    let languageSet = languages[language];
-    if (languageSet) {
-        Object.keys(languageSet).forEach((id) => {
-            console.log("Changing element with ID:", id); // Debugging line
-            document.getElementById(id).innerText = languageSet[id];
-        });
-    }
+  console.log("Changing language to:", language); // Debugging line
+  let languageSet = languages[language];
+  if (languageSet) {
+    Object.keys(languageSet).forEach((id) => {
+      let element = document.getElementById(id);
+      if (element) {
+        element.innerText = languageSet[id];
+      }
+    });
+  }
 }
-element.addEventListener("click", (event) => {
-    event.preventDefault();
-    let language = element.getAttribute("data-change-language");
-    localStorage.setItem("language", language);
-    changeLanguage(language);
-    console.log("Language changed to:", language);
+
+// Add event listeners to language switch links
+document.addEventListener("DOMContentLoaded", () => {
+  let storedLanguage = localStorage.getItem("language");
+  if (storedLanguage) {
+    changeLanguage(storedLanguage);
+  } else {
+    // Set a default language here if needed
+    changeLanguage("en"); // Default to English
+  }
+
+  document.querySelectorAll("[data-change-language]").forEach((element) => {
+    element.addEventListener("click", (event) => {
+      event.preventDefault();
+      let language = element.getAttribute("data-change-language");
+      localStorage.setItem("language", language);
+      changeLanguage(language);
+    });
+  });
 });
